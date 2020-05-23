231b4d3b10f993bbb154a3bf45935163437c0a23
Johannes Schnatterer <johannes.schnatterer@cloudogu.com>
2020-05-23 15:00:30 +0200

    Implement NO_COLOR

--- meta-entrypoint.sh
+++ meta-entrypoint.sh
@@ -6,6 +6,7 @@ export DOMAIN=${DOMAIN:-''}
 LOCAL_HTTP_PORT=${LOCAL_HTTP_PORT:-8080}
 # Use staging to avoid letsencrypt's rate limits!
 STAGING=${STAGING:-'false'}
+NO_COLOR=${NO_COLOR:-''}
 export JAVA_OPTS="-Djava.awt.headless=true -XX:+UseG1GC -Dfile.encoding=UTF-8 -Ddomain=${DOMAIN}" 
 
 BASEDIR=$(dirname $0)
@@ -31,7 +32,7 @@ function fetchCerts() {
    while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:${LOCAL_HTTP_PORT})" -ge 500 ]]; do sleep 1; done
    green "Tomcat is ready."
 
-    trap 'SIG_INT_RECEIVED="true" && green "Stopping certifcate process"' INT 
+    trap 'SIG_INT_RECEIVED="true" && green "Stopping certificate process"' INT 
     
     SIG_INT_RECEIVED='false'
     
@@ -47,16 +48,24 @@ function fetchCerts() {
 }
 
 function green() {
-    echo -e "${GREEN}$@${NO_COLOR}"
+    if [[ -z ${NO_COLOR} ]]; then
+        echo -e "${GREEN}$@${DEFAULT_COLOR}"
+    else 
+        echo "$@"
+    fi
 }
 
 function red() {
-    echo -e "${RED}$@${NO_COLOR}"
+    if [[ -z ${NO_COLOR} ]]; then
+        echo -e "${RED}$@${DEFAULT_COLOR}"
+    else 
+        echo "$@"
+    fi
 }
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-NO_COLOR='\033[0m'
+DEFAULT_COLOR='\033[0m'
 
 
 main "$@"
